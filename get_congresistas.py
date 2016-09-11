5fd90dd59f0aa98a097c1700ec66cf9add04e680
Josue Ortega Caro <josueortc@gmail.com>
2016-09-10 23:56:39 -0500

    Create get_congresistas.py

---
+++ get_congresistas.py
@@ -0,0 +1,6 @@
+
+def get_congresistas(string_input):
+    #Este codigo te permite sacar el nombre de los congresistas que fueron autores en el proyecto de ley, basandome en el formato del API de "Proyectos De Ley"
+    import re
+    result = re.search('"congresistas":(.*)",', string_input)
+    return result.group(1)
